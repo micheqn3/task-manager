68336676f2f27426a623c382a067640d5709dc0d
Michelle Nguyen <michellenguyen11239@aol.com>
2021-09-01 11:48:02 -0700

    Added task-item component

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,17 @@
+// Task-item class. Renders each task item
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
