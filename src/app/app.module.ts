72197a88e75e979bee3f3be51cb9c22995fdc00b
Michelle Nguyen <michellenguyen11239@aol.com>
2021-09-01 10:53:51 -0700

    Added header component in components folder. Reassigned info from main app component to header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 // Import main component
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
