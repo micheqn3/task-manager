a980f4586a00fc7e1a91fa320b455df330a42a48
Michelle Nguyen <michellenguyen11239@aol.com>
2021-09-01 13:12:33 -0700

    Updated task service and added it to tasks component

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -2,9 +2,10 @@
 
 import { Component, OnInit } from '@angular/core';
 
-// Import our task interface and mock tasks
-import { Task } from '../../Task'
-import { TASKS } from '../../mock-tasks'
+// Import our task interface and task service
+import { Task } from '../../Task';
+import { TaskService } from '../../services/task.service';
+
 
 @Component({
   selector: 'app-tasks',
@@ -12,12 +13,18 @@ import { TASKS } from '../../mock-tasks'
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
 
-  constructor() { }
+  // Set tasks initial state
+  tasks: Task[] = [];
+
+  // In order to use a service, need to add it to constructor as an argument
+  constructor(private taskService: TaskService) { }
 
   // Life cycle method that runs when the component loads
+  // Will retrieve the tasks from task service
+  // Subscribe to observable to constantly watch it
+  // This will set the tasks equal to the task we get back from the observable
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => this.tasks = tasks)
   }
-
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,4 +1,9 @@
+// Task service will share data to components
+
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Task } from '../Task';
+import { TASKS } from '../mock-tasks';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +11,10 @@ import { Injectable } from '@angular/core';
 export class TaskService {
 
   constructor() { }
+  
+  // Returns an observable of the tasks
+  getTasks(): Observable<Task[]> {
+    const tasks = of(TASKS);
+    return tasks;
+  }
 }
