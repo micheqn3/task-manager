dd58e6806ac9b633a867852b69194d89d5d09b95
Michelle Nguyen <michellenguyen11239@aol.com>
2021-09-01 14:10:44 -0700

    Added db.json file, new script in package.json, and updated app.module and task-service

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,20 +1,22 @@
 // Task service will share data to components
 
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Task } from '../Task';
-import { TASKS } from '../mock-tasks';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  // Private property for api url
+  private apiUrl = 'http://localhost:5000/tasks';
+
+  constructor(private http: HttpClient) {}
   
   // Returns an observable of the tasks
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
