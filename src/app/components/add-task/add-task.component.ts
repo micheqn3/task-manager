306688178d0f0a6f46d751e5ccce74faf99fcbe3
Michelle Nguyen <michellenguyen11239@aol.com>
2021-09-14 17:33:37 -0700

    Updated add-task component to emit event on form submit

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../Task';
 
 @Component({
   selector: 'app-add-task',
@@ -6,15 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-task.component.css']
 })
 export class AddTaskComponent implements OnInit {
+
+  // Form data
+  // Using ngModel in HTML to allow two way binding
   text: string;
   day: string;
   reminder: boolean = false;
+
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  // On form submit, save form data to an object and emit onAddTask event for parent to see
+  onSubmit() {
 
+    if (!this.text || !this.day) {
+      alert('Please provide all values.');
+      return;
+    }
 
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder
+    }
+
+    this.text = ''
+    this.day = ''
+    this.reminder = false;
+
+    this.onAddTask.emit(newTask);
+  }
 }
